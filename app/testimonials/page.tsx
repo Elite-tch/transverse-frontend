67eff9200be858a0d3c1ee30c0e4654921493d5c
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2024-12-21 09:15:34 +0100

    tiny fix

---
+++ app/testimonials/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Testimonials from '../components/Testimonials';
+
+const Testimonial = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <Testimonials />
+    </div>
+  );
+};
+
+export default Testimonial;
